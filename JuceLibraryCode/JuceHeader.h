768f5051082d7804c709068ccf86fce9beeed70b
Kevan Atkins <kevan.atkins@gmail.com>
2017-02-05 23:40:51 +1100

    Renaming project. Build configuration.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "noise-generator";
+    const char* const  projectName    = "fm-drone-noise-maker";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
